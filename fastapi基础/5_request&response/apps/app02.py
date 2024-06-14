ea5b8c1fa497e5dc6e0e8a63301530706cc5a233
fu-jw <contact@fu-jw.com>
2024-06-14 16:10:49 +0800

    add: 请求与响应-请求体数据类型

--- fastapi基础/5_request&response/apps/app02.py
+++ fastapi基础/5_request&response/apps/app02.py
@@ -7,7 +7,7 @@ app02 = APIRouter()
 
 # 带参路径
 @app02.get("/jobs")
-async def get_items(kd, xl: str, gj: str = "3-5"):
+async def get_item(kd, xl: str, gj: str = "3-5"):
     return {
         "kd": kd,
         "xl": xl,
--- fastapi基础/5_request&response/apps/app03.py
+++ fastapi基础/5_request&response/apps/app03.py
@@ -1,13 +1,40 @@
+from typing import List
+
 from fastapi import APIRouter
-from typing import Union
+from pydantic import BaseModel, Field, field_validator
 from typing import Optional
 
 app03 = APIRouter()
 
 
-# 带参路径
-@app03.post("/data")
-async def data():
-    return {
+# 定义请求体数据类型
+# 继承BaseModel，由pydantic进行数据类型校验
+class User(BaseModel):
+    # 可以有默认值
+    name: str = "root"
+    # 可以有数据范围，默认值，最大最小值
+    age: int = Field(default=20, gt=10, le=100, max=100, min=10)
+    friends: Optional[List[int]]
+    # 可以使用正则表达式
+    # other: str = Field(pattern="^a")
+    other: str
 
-    }
+    # validator 已经废弃
+    # cls 类型对象
+    # v 字段值
+    @field_validator('other')# 要校验的字段
+    def validate_name(cls, v):
+        # 使用断言，如果true就继续
+        # 否则抛出异常提示：Assertion failed, 只能字母
+        assert v.isalpha(), "只能字母"
+        return v
+
+
+# 请求体数据
+@app03.post("/data")
+async def data(user: User):
+    print(user, type(user))  # name='fredo' age=20 friends=[1, 2, 3] <class 'apps.app03.User'>
+    print(user.name)  # fredo
+    print(user.dict())  # {'name': 'fredo', 'age': 20, 'friends': [1, 2, 3]}
+
+    return {}
