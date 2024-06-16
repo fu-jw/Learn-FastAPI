2343cd2195dd8e02ab45bca1a03c693a7873f4de
fu-jw <contact@fu-jw.com>
2024-06-16 10:12:40 +0800

    add: 中间件-CORS

---
+++ fastapi基础/8_CORS/main.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# 一个允许跨域请求的源列表
+origins = [
+    "http://localhost.com",
+    "https://localhost.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
